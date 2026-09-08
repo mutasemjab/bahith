b87763be331aa4bbb98d7fdc329484d42bfe6086
mutasem <mutasemms198@gmail.com>
2026-09-08 22:32:45 +0300

    first commit

--- resources/views/admin/students/edit.blade.php
+++ resources/views/admin/students/edit.blade.php
@@ -43,10 +43,7 @@
                 <label class="form-label">{{ __('messages.new_password') }}</label>
                 <input type="password" name="password" class="form-control" placeholder="{{ __('messages.leave_blank_password') }}">
             </div>
-            <div class="col-md-6">
-                <label class="form-label">{{ __('messages.confirm_password') }}</label>
-                <input type="password" name="password_confirmation" class="form-control">
-            </div>
+         
             <div class="col-md-4">
                 <label class="form-label">{{ __('messages.phone_label') }}</label>
                 <input type="text" name="phone" value="{{ old('phone', $student->phone) }}" class="form-control">
